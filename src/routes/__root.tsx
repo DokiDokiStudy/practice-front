7b7672e9b49e6c1833948cb48e17f6cb3c73b224
yang <diddnjswns20@gmail.com>
2025-08-14 06:43:09 +0900

    add root theme provider

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,12 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import TopNav from "@/components/common/TopNav";
+import { ThemeProvider } from "@/themes/ThemeProvider";
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <ThemeProvider>
       <TopNav />
       <Outlet />
-    </>
+    </ThemeProvider>
   ),
 });
