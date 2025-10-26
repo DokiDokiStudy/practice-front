fb87c4138499f5d877bcaeb8ddd60d9ebbd074fd
yang <diddnjswns20@gmail.com>
2025-10-26 18:10:56 +0900

    main

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,38 @@
 import { CardButton } from "@/shared/ui";
-import { MainBanner } from "@/widgets/main";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Autoplay } from "swiper/modules";
 
-export function Main() {
+export const Main = () => {
+  const bannerImages = [
+    "/images/개발자농담1.png",
+    "/images/just_joke.jpg",
+    "/images/running_curve.jpg",
+  ];
+
   return (
     <div className="flex-1 flex flex-col bg-gradient-to-b from-white via-blue-50 to-blue-100">
-      <MainBanner />
+      <div className="w-full h-[35vh]">
+        <Swiper
+          modules={[Pagination, Autoplay]}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop={true}
+          pagination={{ clickable: true }}
+          className="w-full h-full"
+        >
+          {bannerImages.map((src, idx) => (
+            <SwiperSlide key={idx}>
+              <img
+                src={src}
+                alt={`배너 ${idx + 1}`}
+                className="w-full h-full object-cover"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+
       <div className="flex-1 px-6 py-12 bg-white">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
           <CardButton
@@ -23,4 +51,4 @@ export function Main() {
       </div>
     </div>
   );
-}
+};
